2d74e30c27b0802bd700dd9ccf05af536f21034c
Brandon Colangelo <brandoncancode@gmail.com>
2023-10-07 20:18:18 -0400

    Documentation.

--- flask_app/controllers/controllers_users.py
+++ flask_app/controllers/controllers_users.py
@@ -1,6 +1,9 @@
 from flask_app import app
-from flask import render_template, redirect
+from flask import render_template
 
+
+# Get routes
+# Index route
 @app.get('/')
 def index():
     return render_template('index.html')
