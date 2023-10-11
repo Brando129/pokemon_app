096ca1a3fce63406c36eb069962a9d3115245cbc
Brandon Colangelo <brandoncancode@gmail.com>
2023-10-11 11:26:04 -0400

    Styling the display pokemon page.

--- flask_app/controllers/controllers_users.py
+++ flask_app/controllers/controllers_users.py
@@ -22,15 +22,19 @@ def display_pokemon():
 @app.post('/search/pokemon')
 def search_pokemon():
 
+    # Getting the url for a pokemon
     name = request.form['name']
     url = f'https://pokeapi.co/api/v2/pokemon/{name}/'
     response = requests.get(url)
 
+    # Data for each pokemon
     session['id'] = response.json()['id']
     session['name'] = response.json()['name']
     session['image'] = response.json()['sprites']['other']['official-artwork']['front_default']
     session['height'] = response.json()['height']
     session['weight'] = response.json()['weight']
+    session['type'] = response.json()['types'][0]['type']['name']
     session['ability'] = response.json()['abilities'][0]['ability']['name']
+    session['base_stat'] = response.json()['stats'][0]['base_stat']
 
     return redirect('/display/pokemon/info')
